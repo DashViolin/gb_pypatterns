e13dca0bf49ee93dbe5552f15f2f6cc8c63cb17e
DashViolin <mymail@srv.org>
2023-01-13 09:35:01 +0300

    lesson01 - wsgi-server, page and front controllers (fixed run.py)

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import platform
+import subprocess
 import sys
-from subprocess import Popen
 from time import sleep
 
 command = "python manage.py run"
 if platform.system().lower() == "windows":
-    server_task = Popen(command, creationflags=subprocess.CREATE_NEW_CONSOLE)  # type: ignore
+    server_task = subprocess.Popen(command, creationflags=subprocess.CREATE_NEW_CONSOLE)  # type: ignore
 else:
-    server_task = Popen(command, executable="/bin/bash", shell=True)
+    server_task = subprocess.Popen(command, executable="/bin/bash", shell=True)
 
 while True:
     try:
